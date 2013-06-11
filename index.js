3acca77f379328f71da921b930ff7f68803a5de4
kazunori.kimura <kazunori.kimura.js@gmail.com>
2013-06-11 18:07:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// statics
+
+var fs = require("fs"),
+    ejs = require("ejs"),
+    connect = require("connect");
+
+var ejs_dir = __dirname + "/_ejs",
+    html_dir = __dirname + "/_html";
+
+//---
+
+
+
+//---
+// entry point
+function main(){
+    //startup connect
+}
