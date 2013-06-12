d57c1f05df431760cda7f297c6f6be7955842b46
kazunori.kimura <kazunori.kimura.js@gmail.com>
2013-06-12 14:21:01 +0900

    とりあえず完成

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ statics
 
 ejsから静的なHTMLを生成します。
 
+
+[ejs](https://github.com/visionmedia/ejs)
+
--- index.js
+++ index.js
@@ -3,17 +3,22 @@
 
 var fs = require("fs"),
     ejs = require("ejs"),
-    connect = require("connect");
+    clc = require("cli-color");
 
-var ejs_dir = __dirname + "/_ejs",
-    html_dir = __dirname + "/_html";
+var ejs_dir = __dirname + "\\_ejs",
+    html_dir = __dirname + "\\_html";
 
-//---
+//ejsファイル取得
+fs.readdirSync(ejs_dir).forEach(function(item){
+    if (/\.ejs$/.test(item)){
+        //ejsファイル変換処理
+        var ejs_file = fs.readFileSync(ejs_dir + "\\" + item, "utf-8");
+        var html = ejs.render(ejs_file, {filename: ejs_dir+"\\"+item});
+        //html出力
+        var html_path = html_dir + "\\" + item.replace(/\.ejs$/i, ".html");
+        fs.writeFileSync(html_path, html, {encoding: "utf-8"});
 
+        console.log(clc.blue.bold(ejs_dir+"\\"+item + " -> " + html_path));
+    }
+});
 
-
-//---
-// entry point
-function main(){
-    //startup connect
-}
